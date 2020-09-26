e95baed9ffcd9bb8c2611bdad6f93bd510864b9c
isaia.spinelli <isaia.spinelli@heig-vd.ch>
2020-09-26 10:30:53 +0200

    Start Git + gitignore

---
+++ mbed-os-example-blinky/main.cpp
@@ -0,0 +1,46 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+
+
+// Blinking rate in milliseconds
+#define BLINKING_RATE     500ms
+
+
+void addOne_val(int i) {
+  i++; // Has no effect outside this scope because this is a copy of the original
+}
+
+void addOne_ref(int& i) {
+  i++; // Actually changes the original variable
+}
+
+void addOne(int* i) {
+  (*i)++; // Actually changes the original variable, but i can be null ! 
+}
+
+int main()
+{
+    int i = 0;
+    // Initialise the digital pin LED1 as an output
+    DigitalOut led(LED1);
+
+    while (true) {
+        led = !led;
+        ThisThread::sleep_for(BLINKING_RATE);
+
+        
+  
+        addOne_val(i);
+        printf("value of i is %d\n", i);
+
+        addOne_ref(i);
+        printf("value of i is %d\n", i);
+
+        addOne(&i);
+        printf("value of i is %d\n", i);
+    }
+}
